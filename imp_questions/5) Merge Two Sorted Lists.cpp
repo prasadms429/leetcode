1220c4eccf7a912c7ccc68b0bbfd10f66b8a6178
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-07-13 22:11:16 +0530

    Create 5) Merge Two Sorted Lists.cpp

---
+++ imp_questions/5) Merge Two Sorted Lists.cpp
@@ -0,0 +1,92 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head;
+        ListNode *Last;
+        if(list1==NULL){
+            return list2;
+        }
+        if(list2==NULL){
+            return list1;
+        }
+        if(list1->val < list2->val){
+            head=Last=list1;
+            list1=list1->next;
+            head->next=NULL;
+        }
+        else
+        {
+            head=Last=list2;
+            list2=list2->next;
+            head->next=NULL;
+        }
+        
+        while(list1 && list2)
+        {
+            if(list1->val < list2->val){
+                Last->next=list1;
+                Last=list1;
+                list1=list1->next;
+                Last->next=NULL;
+            }
+            else{
+                Last->next=list2;
+                Last=list2;
+                list2=list2->next;
+                Last->next=NULL;
+            }
+        }
+        
+        if(list1){
+            Last->next=list1;
+        }
+        if(list2){
+            Last->next=list2;
+        }
+        
+        return head;
+        
+    }
+};
+
+
+/* Question -
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+*/
