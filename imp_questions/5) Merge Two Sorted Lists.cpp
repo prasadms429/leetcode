a29ec2aa6ad1592c89e42db9f755cca6d91a23d0
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-07-13 23:16:09 +0530

    Update 5) Merge Two Sorted Lists.cpp

--- imp_questions/5) Merge Two Sorted Lists.cpp
+++ imp_questions/5) Merge Two Sorted Lists.cpp
@@ -11,18 +11,18 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *head;
-        ListNode *Last;
-        if(list1==NULL){
+        ListNode *head;     // create head pointer to hold, address of first node to return.
+        ListNode *Last;     // This is for storing, traversing and for creating sorted merge list.
+        if(list1==NULL){        // If first list is empty, then write second list as it is.
             return list2;
         }
         if(list2==NULL){
             return list1;
         }
-        if(list1->val < list2->val){
+        if(list1->val < list2->val){    // If first node value is less than second node value, then do this. This 'if else' code is for finding first node to point to the head and 'while' code is for all other nodes. 
             head=Last=list1;
-            list1=list1->next;
-            head->next=NULL;
+            list1=list1->next;       // After finding less value, move pointer to next node, for comparing other values.
+            head->next=NULL;        // To hold the address of first node.
         }
         else
         {
@@ -31,13 +31,13 @@ public:
             head->next=NULL;
         }
         
-        while(list1 && list2)
-        {
-            if(list1->val < list2->val){
-                Last->next=list1;
-                Last=list1;
-                list1=list1->next;
-                Last->next=NULL;
+        while(list1 && list2)   // This while loop executes till either list1 or list2 become null, because for some list, after one list becomes empty, 
+        {                       // then other list may contains further nodes, to point that remaining nodes as it is (because that are sorted ) we use 'if' statement code below while loop.
+            if(list1->val < list2->val){    // If list1 node value less than list2 node value.
+                Last->next=list1;       // To do link between Last node of merged list and current node of list1.
+                Last=list1;             // then we move Last pointer to list1 node that is linked to merged list as current last node.
+                list1=list1->next;      // after that we move list1 pointer to next node, for comparing node values between two lists.
+                Last->next=NULL;        // Then we make current last node pointer to point to null.
             }
             else{
                 Last->next=list2;
@@ -47,7 +47,7 @@ public:
             }
         }
         
-        if(list1){
+        if(list1){                  // If list1 is not pointing to null and list1 values are still there, then point next pointer of last node to list1.
             Last->next=list1;
         }
         if(list2){
